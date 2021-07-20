04f4832c7b70fd25841ed6a6a4658dc681a7caf2
Robert Marquess <qowsrule@gmail.com>
2021-07-19 22:07:29 -0400

    Add project files.

---
+++ ZeroFlipDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZeroFlipDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
