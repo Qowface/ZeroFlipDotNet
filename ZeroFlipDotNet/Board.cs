3f8b32fe3452776316c734ed43a56c4c49fa43d9
Robert Marquess <qowsrule@gmail.com>
2021-07-22 15:27:52 -0400

    Generate a board of random tiles and print it out.

--- ZeroFlipDotNet/Board.cs
+++ ZeroFlipDotNet/Board.cs
@@ -1,16 +1,33 @@
-﻿namespace ZeroFlipDotNet
+﻿using System;
+
+namespace ZeroFlipDotNet
 {
     class Board
-    {   
-        public int Width { get; private set; }
-        public int Height { get; private set; }
+    {
+        private static Random _random = new Random();
+
+        public int Rows { get; private set; }
+        public int Cols { get; private set; }
         public Tile[,] Tiles { get; private set; }
 
-        public Board(int width, int height)
+        public Board(int rows, int cols)
         {
-            Width = width;
-            Height = height;    
-            Tiles = new Tile[height, width];
+            Rows = rows;
+            Cols = cols;    
+            Tiles = new Tile[rows, cols];
+
+            Create();
+        }
+
+        public void Create()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Cols; col++)
+                {
+                    Tiles[row, col] = new Tile(_random.Next(4));
+                }
+            }
         }
     }
 }
--- ZeroFlipDotNet/Program.cs
+++ ZeroFlipDotNet/Program.cs
@@ -7,7 +7,17 @@ namespace ZeroFlipDotNet
         static void Main(string[] args)
         {
             Board board = new Board(5, 5);
-            Console.WriteLine($"Board created with {board.Tiles.Length} tiles ({board.Height} rows, {board.Width} cols)");
+
+            Console.WriteLine($"Board created with {board.Tiles.Length} tiles ({board.Rows} rows, {board.Cols} cols)");
+
+            for (int row = 0; row < board.Rows; row++)
+            {
+                for (int col = 0; col < board.Cols; col++)
+                {
+                    Console.Write(board.Tiles[row, col].Value + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
